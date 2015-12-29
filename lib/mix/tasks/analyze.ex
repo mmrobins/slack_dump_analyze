f2503cd21de7371ff86b8d7516e6ccdb4c18be8e
Matt Robinson <matt@mattrobinson.net>
2015-12-28 23:36:20 -0600

    wip

--- lib/mix/tasks/analyze.ex
+++ lib/mix/tasks/analyze.ex
@@ -3,8 +3,26 @@ defmodule Mix.Tasks.Analyze do
 
   def run(_) do
     require IEx
-    {:ok, t} = File.read "../users.json"
+    File.cd("..")
+
+    {:ok, t} = File.read "users.json"
     {:ok, users} = Poison.decode(t)
+
+    user_map = users
+    |> Enum.map(fn(u) -> {u["id"], u["name"]} end)
+    |> Enum.into(%{})
+
+    files = Path.wildcard("**/*.json")
+    files
+    mapped_files = Enum.map(files, fn(f) ->
+      Task.async(fn ->
+        {:ok, t} = File.read(f)
+        {:ok, day} = Poison.decode(t)
+        day
+      end)
+    end)
+    parsed_files = Enum.map(mapped_files, &Task.await/1)
+    IEx.pry
   end
 
 end
